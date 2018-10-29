ae0c341e3234b2557790032b7dccb8e8b60cd56b
Brett Taylor <btaylor@cloudmineinc.com>
2018-10-29 13:46:12 -0400

    update var

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ set -e
 base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 COMMIT_NUM=$(git rev-list --count --first-parent HEAD)
-version=1.${COMMIT_NUM}
+VERSION=1.${COMMIT_NUM}
 
-echo "Building docker images for packer_awscli ${version}..."
-docker build -t ssplatt/packer_awscli:${version} .
-docker tag ssplatt/packer_awscli:${version} ssplatt/packer_awscli:latest
+echo "Building docker images for packer_awscli ${VERSION}..."
+docker build -t ssplatt/packer_awscli:${VERSION} .
+docker tag ssplatt/packer_awscli:${VERSION} ssplatt/packer_awscli:latest
 
-echo "Uploading docker images for packer_awscli ${version}..."
-docker push ssplatt/packer_awscli:${version}
+echo "Uploading docker images for packer_awscli ${VERSION}..."
+docker push ssplatt/packer_awscli:${VERSION}
 docker push ssplatt/packer_awscli:latest
